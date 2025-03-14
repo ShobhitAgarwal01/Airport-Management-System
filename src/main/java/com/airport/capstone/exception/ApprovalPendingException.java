c1bb7c78f07f1d684b80c51cc74c3d213554bb0f
guSamyuktha <103623425+guSamyuktha@users.noreply.github.com>
2025-03-14 10:23:51 +0530

    Add files via upload

---
+++ src/main/java/com/airport/capstone/exception/ApprovalPendingException.java
@@ -0,0 +1,8 @@
+package com.airport.capstone.exception;
+
+public class ApprovalPendingException extends RuntimeException{
+
+	public ApprovalPendingException(String message){
+        super(message);
+    }
+}
